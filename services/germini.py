b2e7efc28bc9acbcf39ec9565e3530aed1f2ee72
Francisco C. <debian.programmer.francisco@gmail.com>
2025-08-25 08:42:38 -0300

    adicionando mais logicas de AI

--- services/germini.py
+++ services/germini.py
@@ -1,15 +1,21 @@
-from google import genai
+from google import generativeai as genai
 
 api_key = 'AIzaSyBU5alLRsjSR5qUYaEntVvfRHtxHAUMDY8'
 
-cliente = genai.Client(api_key=api_key)
+genai.configure(api_key=api_key)
+genai_config = genai.types.GenerationConfig(
+  temperature=0.9,
+  candidate_count=1
+)
+genai_model = genai.GenerativeModel('gemini-2.5-flash')
+chat = genai_model.start_chat()
 
-def test():
-  response = cliente.models.generate_content(
-    model='gemini-2.5-flash',
-    contents="Fale que isso é um teste de conexão"
-  )
+def Enviar_Mensagem(mensagem:str):
+  response = chat.send_message(mensagem)
+  return response
 
-  print(response.text)
+def historico():
+  return chat.history
 
-test()
+print(Enviar_Mensagem("""Você é uma assistente de um pesquisador ou estudante que busca fazer sistemas embarcados para microcontroladores.
+                Você deve gerar códigos, se solicitado pelo usuário e explicalos. Suas respostas devem obedecer a sintaxe de MarkDown. Por exemplo, crie um hello world de sistemas embarcados."""))
