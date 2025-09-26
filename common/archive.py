bfcc909cd065d62b956df81425ed82bb6183cdc0
Francisco C <debian.programmer.francisco@gmail.com>
2025-09-26 15:05:50 -0300

    alterandno funções de criação de arquivos bat

--- common/archive.py
+++ common/archive.py
@@ -1,20 +1,15 @@
-import os
+import os, subprocess
 
 def criar_diretorios():
     """Cria os diretórios 'temporario' e 'executavel' se eles não existirem."""
     uri = r"C:\Users\franc\Documents"
-    diretorio_temporario = os.path.join(uri, 'temporario')
     diretorio_executavel = os.path.join(uri, 'executavel')
-
-    if not os.path.exists(diretorio_temporario):
-        os.makedirs(diretorio_temporario)
-        print(f"Diretório '{diretorio_temporario}' criado com sucesso.")
 
     if not os.path.exists(diretorio_executavel):
         os.makedirs(diretorio_executavel)
         print(f"Diretório '{diretorio_executavel}' criado com sucesso.")
 
-def salvar_arquivo_temporario(nome_arquivo: str, conteudo: str):
+def salvar_arquivo(nome_arquivo: str, conteudo: str):
     """
     Salva o conteúdo em um arquivo no diretório 'temporario'.
     O arquivo é sobrescrito se já existir.
@@ -23,8 +18,8 @@ def salvar_arquivo_temporario(nome_arquivo: str, conteudo: str):
         nome_arquivo (str): O nome do arquivo a ser salvo (ex: 'meu_codigo.c').
         conteudo (str): O conteúdo a ser escrito no arquivo.
     """
-    diretorio_temporario = 'temporario'
-    caminho_completo = os.path.join(diretorio_temporario, nome_arquivo)
+    diretorio_executavel = 'executavel'
+    caminho_completo = os.path.join(diretorio_executavel, nome_arquivo)
     
     try:
         with open(caminho_completo, 'w', encoding='utf-8') as f:
@@ -32,3 +27,31 @@ def salvar_arquivo_temporario(nome_arquivo: str, conteudo: str):
         print(f"Arquivo '{nome_arquivo}' salvo com sucesso em '{caminho_completo}'.")
     except IOError as e:
         print(f"Erro ao salvar o arquivo '{nome_arquivo}': {e}")
+
+def criar_arquivo_bat():
+    uri = r"C:\Users\franc\Documents"
+
+    content = """
+    @echo off
+    echo Verificando existencia da arduino-cli
+    echo.
+    arduino-cli --version
+    IF %ERRORLEVEL% NEQ 0 (
+        echo Arduino CLI nao instalado
+        exit 
+    ) ELSE {
+        echo Arduino CLI instalado
+    }
+    pause
+    """
+
+    with open(uri / "executavel", encoding='UTF-8', mode='w') as arq:
+        arq.write(content)
+
+    print(f'Arquivo bat criado com exito')
+
+def execute_bat():
+    resultado = subprocess.run("C:/Users/franc/Desenvolvendo/com/francosmonx/python/bat/exemplo.bat", capture_output=True, text=True, shell=True)
+
+    print("Saída do arquivo .bat:")
+    print(resultado.stdout)  # Exibe a saída padrão
