7b15e0541951cf546881565c1151d9498af34f53
Francisco C <debian.programmer.francisco@gmail.com>
2026-06-15 23:38:17 -0300

    adicionando mais microcontroladores na lista

--- models/configuracao.sql
+++ models/configuracao.sql
@@ -39,8 +39,16 @@ INSERT OR IGNORE INTO ia(nome_ia, modelo_disponivel) VALUES ("Gemini", "gemini-3
 INSERT OR IGNORE INTO ia(nome_ia, modelo_disponivel) VALUES ("Gemini", "gemini-3.5-flash");
 INSERT OR IGNORE INTO ia(nome_ia, modelo_disponivel) VALUES ('ChatGPT', "gpt-4o");
 INSERT OR IGNORE INTO ia(nome_ia, modelo_disponivel) VALUES ('ChatGPT', "gpt-4o-mini");
--- INSERT OR IGNORE INTO ia(nome_ia, modelo_disponivel) VALUES ("ChatGPT", "gpt-3.5-turbo");
--- INSERT OR IGNORE INTO ia(nome_ia, modelo_disponivel) VALUES ("ChatGPT", "gpt-4-turbo");
 
 INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("ESP32S NodeMCU-32S", "esp32:esp32:nodemcu-32s", "esp32:esp32", 0);
-INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("Arduino UNO R3", "arduino:avr:uno", "arduino:avr", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("Arduino UNO", "arduino:avr:uno", "arduino:avr", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("Arduino UNO Mini", "arduino:avr:unomini", "arduino:avr", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("Arduino UNO WiFi", "arduino:avr:unowifi", "arduino:avr", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("Arduino Leonardo", "arduino:avr:leonardo", "arduino:avr", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("Arduino Mega", "arduino:avr:mega", "arduino:avr", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("Arduino Mega 2560", "arduino:avr:mega", "arduino:avr", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("LilyPad Arduino", "arduino:avr:lilypad", "arduino:avr", 0);
+
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("ESP32S2 Dev Module", "esp32:esp32:esp32s2", "esp32:esp32", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("ESP32S3 Dev Module", "esp32:esp32:esp32s3", "esp32:esp32", 0);
+INSERT OR IGNORE INTO microcontrolador(nome, fqbn, package_id, ambiente_configurado) VALUES ("LilyPad Arduino", "arduino:avr:lilypad", "arduino:avr", 0);
